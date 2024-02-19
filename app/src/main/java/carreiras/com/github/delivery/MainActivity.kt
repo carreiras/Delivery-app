6b454b8cbf56d1cf7a91cfb6e813b3f76016f028
Ewerton Carreira <ewertoncarreira@gmail.com>
2024-02-19 17:16:03 -0300

    Criando o item do produto #2

--- app/src/main/java/carreiras/com/github/delivery/MainActivity.kt
+++ app/src/main/java/carreiras/com/github/delivery/MainActivity.kt
@@ -3,20 +3,18 @@ package carreiras.com.github.delivery
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import carreiras.com.github.delivery.ui.theme.DeliveryappTheme
@@ -27,11 +25,26 @@ class MainActivity : ComponentActivity() {
         setContent {
             DeliveryappTheme {
                 Surface {
-
+                    ProductItem()
                 }
             }
         }
     }
 }
 
-
+@Preview(showBackground = true)
+@Composable
+fun ProductItem() {
+    Column {
+        Box(modifier = Modifier
+            .height(50.dp)
+            .background(Color.Blue)
+            .width(50.dp))
+        Image(
+            painter = painterResource(id = R.drawable.ic_launcher_background),
+            contentDescription = null
+        )
+        Text(text = "Texto 1")
+        Text(text = "Texto 2")
+    }
+}
