a1dd28af73e95435c1a95d72a7cabc6b116f7c03
Ewerton Carreira <ewertoncarreira@gmail.com>
2024-02-19 14:01:05 -0300

    Explorando o Preview #2

--- app/src/main/java/carreiras/com/github/delivery/MainActivity.kt
+++ app/src/main/java/carreiras/com/github/delivery/MainActivity.kt
@@ -3,21 +3,19 @@ package carreiras.com.github.delivery
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.tooling.preview.Preview
-import carreiras.com.github.delivery.ui.theme.DeliveryappTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Text(text = "My first Composable")
+            MyFirstComposable()
         }
     }
+}
+
+@Composable
+fun MyFirstComposable() {
+    Text(text = "My first Composable")
 }
