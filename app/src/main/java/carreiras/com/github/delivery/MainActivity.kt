377415105439513759999d0bbe5bce1937b879db
Ewerton Carreira <ewertoncarreira@gmail.com>
2024-02-19 14:52:33 -0300

    Utilizando Composables de Layout #2

--- app/src/main/java/carreiras/com/github/delivery/MainActivity.kt
+++ app/src/main/java/carreiras/com/github/delivery/MainActivity.kt
@@ -1,9 +1,9 @@
 package carreiras.com.github.delivery
 
-import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -20,6 +20,15 @@ class MainActivity : ComponentActivity() {
                 }
             }
         }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun ColumnPreview() {
+    Column {
+        Text("Texto 1")
+        Text("Texto 2")
     }
 }
 
