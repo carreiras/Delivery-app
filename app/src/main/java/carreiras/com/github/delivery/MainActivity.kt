833df0caa2b702df3960e7aaa4bfedd692ad3223
Ewerton Carreira <ewertoncarreira@gmail.com>
2024-02-19 16:03:58 -0300

    Personalizando com o Modifier #1

--- app/src/main/java/carreiras/com/github/delivery/MainActivity.kt
+++ app/src/main/java/carreiras/com/github/delivery/MainActivity.kt
@@ -6,10 +6,13 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import carreiras.com.github.delivery.ui.theme.DeliveryappTheme
 
 class MainActivity : ComponentActivity() {
@@ -55,7 +58,7 @@ fun BoxPreview() {
 @Preview(showBackground = true)
 @Composable
 fun CustomLayoutPreview() {
-    Column {
+    Column(Modifier.padding(all = 8.dp)) {
         Text("Texto 1")
         Text("Texto 2")
         Row {
