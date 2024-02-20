24d2a73702c1ec7098d00ba010830481c8cf9651
Ewerton Carreira <ewertoncarreira@gmail.com>
2024-02-20 19:11:08 -0300

    Ajustando os textos #1

--- app/src/main/java/carreiras/com/github/delivery/MainActivity.kt
+++ app/src/main/java/carreiras/com/github/delivery/MainActivity.kt
@@ -7,17 +7,31 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment.Companion.BottomCenter
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.tooling.preview.datasource.LoremIpsum
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import carreiras.com.github.delivery.ui.theme.DeliveryappTheme
+import carreiras.com.github.delivery.ui.theme.Purple500
+import carreiras.com.github.delivery.ui.theme.Teal200
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -35,16 +49,44 @@ class MainActivity : ComponentActivity() {
 @Preview(showBackground = true)
 @Composable
 fun ProductItem() {
-    Column {
-        Box(modifier = Modifier
-            .height(50.dp)
-            .background(Color.Blue)
-            .width(50.dp))
-        Image(
-            painter = painterResource(id = R.drawable.ic_launcher_background),
-            contentDescription = null
-        )
-        Text(text = "Texto 1")
-        Text(text = "Texto 2")
+    Column(
+        Modifier
+            .height(250.dp)
+            .width(200.dp)
+    ) {
+        Box(
+            modifier = Modifier
+                .height(100.dp)
+                .background(brush = Brush.horizontalGradient(colors = listOf(Purple500, Teal200)))
+                .fillMaxWidth()
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.ic_launcher_background),
+                contentDescription = null,
+                Modifier
+                    .size(100.dp)
+                    .offset(y = 50.dp)
+                    .clip(shape = CircleShape)
+                    .align(BottomCenter)
+            )
+        }
+        Spacer(modifier = Modifier.height(50.dp))
+
+        Column (Modifier.padding(16.dp)){
+
+            Text(
+                text = LoremIpsum(50).values.first(),
+                fontSize = 18.sp,
+                fontWeight = FontWeight(700),
+                maxLines = 2,
+                overflow = TextOverflow.Ellipsis
+
+            )
+            Text(
+                text = "14,99",
+                fontSize = 14.sp,
+                fontWeight = FontWeight(400)
+            )
+        }
     }
 }
