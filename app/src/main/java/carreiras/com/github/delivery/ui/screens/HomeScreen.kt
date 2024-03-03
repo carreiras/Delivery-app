41acb30d60d9b2f1acb21c8abb42e79669651dfb
Ewerton Carreira <ewertoncarreira@gmail.com>
2024-03-03 20:25:29 -0300

    Modificando o Design com Slot API

--- app/src/main/java/carreiras/com/github/delivery/ui/screens/HomeScreen.kt
+++ app/src/main/java/carreiras/com/github/delivery/ui/screens/HomeScreen.kt
@@ -1,13 +1,19 @@
 package carreiras.com.github.delivery.ui.screens
 
-import android.util.Log
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.Icon
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -26,11 +32,27 @@ fun HomeScreen(
     sections: Map<String, List<Product>>
 ) {
     Column {
-        var text by remember {mutableStateOf("")}
+        var text by remember { mutableStateOf("") }
 
-        OutlinedTextField(value = text, onValueChange = { newValue ->
-            text = newValue
-        })
+        OutlinedTextField(
+            value = text,
+            onValueChange = { newValue ->
+                text = newValue
+            },
+            Modifier
+                .padding(start = 16.dp, top = 16.dp, end = 16.dp)
+                .fillMaxWidth(),
+            shape = RoundedCornerShape(100),
+            leadingIcon = {
+                Icon(Icons.Default.Search, contentDescription = "Ícone de luoa")
+            },
+            label = {
+                Text(text = "Produto")
+            },
+            placeholder = {
+                Text(text = "O que você procura?")
+            }
+        )
         LazyColumn(
             Modifier
                 .fillMaxSize(),
