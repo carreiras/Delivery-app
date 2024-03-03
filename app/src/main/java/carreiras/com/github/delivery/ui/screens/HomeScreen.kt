6c5dd48b50ba3833c83284bd01b6a128fa9dee8e
Ewerton Carreira <ewertoncarreira@gmail.com>
2024-03-03 20:00:32 -0300

    Utilizando o State no campo de texto

--- app/src/main/java/carreiras/com/github/delivery/ui/screens/HomeScreen.kt
+++ app/src/main/java/carreiras/com/github/delivery/ui/screens/HomeScreen.kt
@@ -4,13 +4,15 @@ import android.util.Log
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Surface
-import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -24,8 +26,10 @@ fun HomeScreen(
     sections: Map<String, List<Product>>
 ) {
     Column {
-        OutlinedTextField(value = "Ewerton", onValueChange = { newValue ->
-            Log.i("HomeScreen", "HomeScreen: TextField $newValue")
+        var text by remember {mutableStateOf("")}
+
+        OutlinedTextField(value = text, onValueChange = { newValue ->
+            text = newValue
         })
         LazyColumn(
             Modifier
