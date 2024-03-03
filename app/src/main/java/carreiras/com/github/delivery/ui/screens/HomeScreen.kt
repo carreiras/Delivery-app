df4d35a1d7d22fa831b08103e4c1988dd3106566
Ewerton Carreira <ewertoncarreira@gmail.com>
2024-03-03 19:15:28 -0300

    Utilizando Lazy nas seções #1

--- app/src/main/java/carreiras/com/github/delivery/ui/screens/HomeScreen.kt
+++ app/src/main/java/carreiras/com/github/delivery/ui/screens/HomeScreen.kt
@@ -1,11 +1,9 @@
 package carreiras.com.github.delivery.ui.screens
 
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.verticalScroll
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -20,22 +18,28 @@ import carreiras.com.github.delivery.ui.theme.DeliveryappTheme
 fun HomeScreen(
     sections: Map<String, List<Product>>
 ) {
-    Column(
+    LazyColumn(
         Modifier
-            .fillMaxSize()
-            .verticalScroll(rememberScrollState()),
+            .fillMaxSize(),
         verticalArrangement = Arrangement.spacedBy(16.dp)
     ) {
-        Spacer(Modifier)
+        item {
+            Spacer(Modifier)
+
+        }
         for (section in sections) {
             val title = section.key
             val products = section.value
-            ProductsSection(
-                title = title,
-                products = products
-            )
+            item {
+                ProductsSection(
+                    title = title,
+                    products = products
+                )
+            }
         }
-        Spacer(Modifier)
+        item {
+            Spacer(Modifier)
+        }
     }
 }
 
